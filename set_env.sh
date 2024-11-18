7d1ce529999c355bfcb5a4fa1fadcda21bfe0fb8
letienlocvn <letienlocvn@gmail.com>
2024-11-18 15:18:03 +0700

    edit env

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=testing
-export POSTGRES_HOST=database-udacity.cmc1sxlcey3u.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRES_PASSWORD=LeTienLoc74123.0
+export POSTGRES_HOST=db-postgre-udacity3.cpilnpwxuote.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres_db
 export AWS_BUCKET=arn:aws:s3:::udacity-s3-dev
 export AWS_REGION=us-east-1
-export AWS_PROFILE=test
+export AWS_PROFILE=arn:aws:iam::749338108763:user/loclt7-course2-udagram
 export JWT_SECRET=testing
 export URL=http://localhost:8100
